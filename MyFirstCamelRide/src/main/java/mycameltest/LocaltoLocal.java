e9501186eefa34c5f7e415af7ba6bbb69b823da9
Hamid Habibi <Hamid.Habibi@ipsoft.com>
2019-09-23 21:19:19 -0400

    Adding camel property placeholders in both Java and XML DSL

--- MyFirstCamelRide/src/main/java/mycameltest/LocaltoLocal.java
+++ MyFirstCamelRide/src/main/java/mycameltest/LocaltoLocal.java
@@ -4,6 +4,7 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 
@@ -13,23 +14,28 @@ import org.apache.camel.impl.DefaultCamelContext;
  * we also have a simple processor
  */
 
-public class LocaltoLocal {
+public class LocalToLocal {
 
     public static void main(String[] args) throws Exception{
 
         CamelContext context = new DefaultCamelContext();
 
+//      Properties component, setting a properties file
+        PropertiesComponent prob = context.getComponent("properties",PropertiesComponent.class);
+        prob.setLocation("classpath:properties.properties");
+
+
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
 //                System.out.println("sdg");
-                    from("file://data/input?noop=true")
+                    from("file:{{source}}?noop=true")
                         .process(new Processor() {
                             public void process(Exchange exchange) throws Exception {
                                 System.out.println("Download completed "+ exchange.getIn().getHeader("CamelFileName"));
                             }
                         })
-                        .to("file://data/output");
+                        .to("file://{{destination}}");
 
             }
         });
